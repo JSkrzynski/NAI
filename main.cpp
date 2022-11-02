675704af1ebf84da1ee7431c556e5fc2fd492b26
JSkrzynski <s23132@pjwstk.edu.pl>
2022-11-02 14:31:44 +0100

    added ex3

--- main.cpp
+++ main.cpp
@@ -1,75 +1,65 @@
 #include <iostream>
-#include <functional>
-#include <map>
-#include <math.h>
+#include <utility>
+#include <vector>
 #include <random>
-#include <time.h>
 
 using namespace std;
-using myfunction_t = std::function<double(double, double)>;
-std::random_device rd;
-std::mt19937 mt_generator(rd());
 
+random_device rd;
+mt19937 mt_generator(rd());
 
-double optimize(myfunction_t function, vector<double> domain, int maxIterations=10000){
-    clock_t startClock, endClock;
-    startClock = clock();
-    double time;
-    uniform_real_distribution<double> dist(domain.at(0), domain.at(1));
-    double currentBest = function(domain.at(0), domain.at(1));
+vector<vector<int>> genotypeGenerator(int size) {
+    vector<vector<int>> genotype;
+    vector<int> chromosomes;
 
-    for(int i = 0; i < maxIterations; i++){
-        double rand1 = dist(mt_generator);
-        double rand2 = dist(mt_generator);
-        double temp;
-        temp = function(rand1,rand2);
+    for (int i = 0; i < size; ++i) {
+        chromosomes.clear();
 
-        if(temp < currentBest){
-            currentBest = temp;
+        for (int j = 0; j < size ; ++j) {
+            uniform_int_distribution<int> uni(0, 1);
+            chromosomes.push_back(uni(mt_generator));
         }
+
+        genotype.push_back(chromosomes);
     }
 
-    endClock = clock();
-    time = ((double) (endClock - startClock)) / CLOCKS_PER_SEC;
-    cout<<"Iterations: "<<maxIterations<<" Time taken: "<<time<<" Lowest: ";
-    return currentBest;
+    return genotype;
 }
 
-int main(int argc, char **argv){
+pair<double, double> decodePhenotype(vector<int> chromosomes) {
+    double x, y = 0.0;
 
-    map<string, myfunction_t> myFunctions;
-    map<string, vector<double>> domain;
-
-    myFunctions["beale"] = [](double x, double y) {
-        return (pow((1.5 - x + (x * y)),2)) + (pow(2.25 - x + (x * pow(y,2)),2))+(pow(2.625 - x + x * pow(y,3),2));
-    };
-    myFunctions["himmel"] = [](double x, double y) {
-        return pow(pow(x,2) + y - 11,2) + pow(x + pow(y,2) - 7,2);
-    };
-    myFunctions["threeHumpCamel"] = [](double x, double y) {
-        return ((2 * pow(x,2)) - (1.05 * pow(x,4)) + ((pow(x,6))/6) + (x*y) + (pow(y,2)));
-    };
-    myFunctions["matyas"] = [](double x, double y) {
-        return (0.26 * (pow(x, 2) + pow(y, 2)) - (0.48 * x * y));
-    };
-
-    domain["beale"] = {-4.5,4.5};
-    domain["himmel"] = {-5,5};
-    domain["threeHumpCamel"] = {-5,5};
-    domain["matyas"] = {-10,10};
-
-    try {
-        vector<string> arguments(argv, argv + argc);
-        auto selectedFunction = arguments.at(1);
-        for(int i = 0; i < 10; i++){
-            cout<<optimize(myFunctions.at(selectedFunction),domain.at(selectedFunction),10000)<<endl;
-        }
+    for (int i = 0; i < chromosomes.size() / 2; i++) {
+        x = x * 2 + chromosomes[i];
     }
-    catch (std::out_of_range aor) {
-        cout << "Error, possible arguments:  ";
-        for (auto [k, v] : myFunctions) cout << k << ", ";
-        cout << endl;
-        return 1;
+
+    for (int i = (int) chromosomes.size() / 2; i < chromosomes.size(); i++) {
+        y = y * 2 + chromosomes[i];
     }
+
+    x = x / pow(2.0, (chromosomes.size() / 2 - 4)) - 8;
+    y = y / pow(2.0, (chromosomes.size() / 2 - 4)) - 8;
+
+    return {x, y};
+}
+
+auto beale = [](pair<double, double> pair) {
+    double x = pair.first;
+    double y = pair.second;
+
+    return (pow((1.5 - x + (x * y)),2)) + (pow(2.25 - x + (x * pow(y,2)),2))+(pow(2.625 - x + x * pow(y,3),2));
+};
+
+
+double fitness(vector<int> chromosomes) {
+    return 1.0 / (1.0 + abs(beale(decodePhenotype(move(chromosomes)))));
+}
+
+int main() {
+    for (auto &element: genotypeGenerator(100 + (23132 % 10) * 2)) {
+        auto decoded = decodePhenotype(element);
+        cout << "X: " << decoded.first << "\nY: " << decoded.second << "\nMaksymalizacja: " << fitness(element) <<"\n"<< endl;
+    }
+
     return 0;
 }
